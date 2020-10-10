dc55a784e1c6ce3960e026053767df5e796f15de
Demid Kashirin <demid.kashirin@gmail.com>
2020-10-10 18:19:48 +0300

    Configure sample http endpoint

--- src/vat_rates_http/core.clj
+++ src/vat_rates_http/core.clj
@@ -1,6 +1,33 @@
-(ns vat-rates-http.core)
+(ns vat-rates-http.core
+  (:require [io.pedestal.http.route :as route]
+            [io.pedestal.http :as http]
+            [vat-rates-http.db :as db]
+            [clojure.java.jdbc :as j]))
+
+(defn respond-hello [req]
+  (prn req)
+  {:status 200 :body (j/query (:db req) ["show tables"])})
+
+(def attach-db
+  {:name ::attach-database
+   :enter (fn [context]
+            (let [conn (db/pool {})]
+              (assoc-in context [:request :db] conn)))})
+
+(def routes
+  (route/expand-routes
+   #{["/index" :get [attach-db #'respond-hello] :route-name :index]}))
+
+(defonce server (atom nil))
+
+(defn start-server []
+  (when (empty? @server)
+    (reset! server (http/start (http/create-server
+                                {::http/routes routes
+                                 ::http/type :jetty
+                                 ::http/port 3000})))))
 
 (defn -main [& args]
-  (println "Hello"))
+  (start-server))
 
-(+ 1 1)
+(comment (-main))
--- src/vat_rates_http/db.clj
+++ src/vat_rates_http/db.clj
@@ -3,18 +3,16 @@
   (:import (com.zaxxer.hikari HikariConfig HikariDataSource)
            (java.util Properties)))
 
-(defn create-pool [opts]
-  (let [ds (HikariDataSource.)]
-    (.setMaximumPoolSize ds 10)
-    (.setDriverClassName ds "org.mariadb.jdbc.Driver")
-    (.setJdbcUrl ds "jdbc:mariadb://localhost:3306/vat_rates_test")
-    (.addDataSourceProperty ds "user" (System/getenv "MYSQL_USER"))
-    (.addDataSourceProperty ds "password" (System/getenv "MYSQL_ROOT_PASSWORD"))
+(defonce db (atom nil))
 
-    {:datasource ds}))
+(defn pool [opts]
+  (if (empty? @db)
+    (let [ds (HikariDataSource.)]
+      (.setMaximumPoolSize ds 10)
+      (.setDriverClassName ds "org.mariadb.jdbc.Driver")
+      (.setJdbcUrl ds "jdbc:mariadb://localhost:3306/vat_rates_test")
+      (.addDataSourceProperty ds "user" (System/getenv "MYSQL_USER"))
+      (.addDataSourceProperty ds "password" (System/getenv "MYSQL_ROOT_PASSWORD"))
 
-(def db
-  (create-pool {}))
-
-(j/query db
-         ["show tables"])
+      (reset! db {:datasource ds}))
+    @db))
